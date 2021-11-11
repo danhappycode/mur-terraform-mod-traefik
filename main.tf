973ff7b2636520a288f3c638eb1bde6f85c4d1d8
Daniel Niedermühlbichler <daniel.niedermuehlbichler@gmail.com>
2021-11-11 14:26:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+    command     = "aws"
+  }
+}
+
 provider "helm" {
   kubernetes {
-    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      command     = "aws-iam-authenticator"
-      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+      command     = "aws"
     }
   }
 }
